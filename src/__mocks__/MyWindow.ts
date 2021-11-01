eba4319bb17db10e3829409ecbe9883d3203c783
Kevin Frei <kevinfrei@hotmail.com>
2021-11-01 10:40:03 -0700

    Updated dependencies and fixed subsequent issues

--- src/__mocks__/MyWindow.ts
+++ src/__mocks__/MyWindow.ts
@@ -105,3 +105,27 @@ export async function InvokeMain(
       return await MockWrite(key);
   }
 }
+
+export async function CallMain(
+  channel: string,
+  key?: string,
+): Promise<string | void> {
+  switch (channel) {
+    case 'read-from-storage':
+      return await MockRead(key);
+    case 'write-to-storage':
+      return await MockWrite(key);
+  }
+}
+
+export async function PostMain(
+  channel: string,
+  key?: string,
+): Promise<string | void> {
+  switch (channel) {
+    case 'read-from-storage':
+      return await MockRead(key);
+    case 'write-to-storage':
+      return await MockWrite(key);
+  }
+}
